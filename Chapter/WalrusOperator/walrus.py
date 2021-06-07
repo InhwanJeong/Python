ab58157af44200dbb72aec9d238708bc49a1a404
inan <ghjklla007@naver.com>
2021-06-07 19:54:58 +0900

    [ADD]walrus operator, 대입식을 간단하게

---
+++ Chapter/WalrusOperator/walrus.py
@@ -0,0 +1,35 @@
+# walrus operator
+
+fruit = {
+    'apple': 10,
+    'lemon': 0,
+    'orange': 5
+}
+'''
+Don't use walrus operator
+a = fruit.get('lemon')
+if a>5:
+'''
+if (a := fruit.get('lemon')) > 5:
+    print('get item')
+else:
+    print('out of stock')
+
+
+# walrus if-else 2
+if (a := fruit.get('lemon')) > 1:
+    print('get lemon juice')
+elif (a := fruit.get('apple')) > 4:
+    print('get apple icecream')
+elif (a := fruit.get('orange')) > 3:
+    print('get orange cand')
+else:
+    print('out of stock')
+
+
+# while walrus
+item = 'apple'
+
+while (itemCount := fruit.get(item)) > 0:
+    print(itemCount)
+    fruit[item] = itemCount -1
